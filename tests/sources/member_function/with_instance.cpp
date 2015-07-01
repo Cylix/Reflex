27db7a0f363f359c00d807723317831d5872c469
Simon Ninon <simon.ninon@gmail.com>
2015-07-01 21:59:21 +0100

    fix tests release under debian (missing pthread library link and class name conflict in tests)

--- tests/sources/member_function/with_instance.cpp
+++ tests/sources/member_function/with_instance.cpp
@@ -1,13 +1,13 @@
 #include <gtest/gtest.h>
 #include <cpp_reflection/cpp_reflection.hpp>
 
-class some_class {
+class reflectable_class {
 private:
     unsigned int nb;
 
 public:
     //! ctor
-    some_class(void) : nb(42) {}
+    reflectable_class(void) : nb(42) {}
 
 public:
     unsigned int get_42(void) { return 42; }
@@ -21,45 +21,45 @@ public:
     static void static_fct(void) {}
 };
 
-REGISTER_CLASS_FUNCTIONS(some_class, (get_42)(do_something)(add)(get_nb)(set_nb)(static_fct))
+REGISTER_CLASS_FUNCTIONS(reflectable_class, (get_42)(do_something)(add)(get_nb)(set_nb)(static_fct))
 
 TEST(MemberFunctionReflectionWithInstance, ReturnGoodValue) {
-    some_class obj;
-    EXPECT_EQ(42, cpp_reflection::reflection_maker<unsigned int()>::invoke(&obj, "some_class", "get_42"));
+    reflectable_class obj;
+    EXPECT_EQ(42, cpp_reflection::reflection_maker<unsigned int()>::invoke(&obj, "reflectable_class", "get_42"));
 }
 
 TEST(MemberFunctionReflectionWithInstance, AcceptMultipleParams) {
     int nb = 0;
 
-    some_class obj;
-    cpp_reflection::reflection_maker<void(const std::string&, int&, char, float, int)>::invoke(&obj, "some_class", "do_something",
+    reflectable_class obj;
+    cpp_reflection::reflection_maker<void(const std::string&, int&, char, float, int)>::invoke(&obj, "reflectable_class", "do_something",
                                                                                                std::string("hello"), nb, 'a', 1.0, 42);
 
     EXPECT_EQ(42, nb);
 }
 
 TEST(MemberFunctionReflectionWithInstance, UpdateObject) {
-    some_class obj;
+    reflectable_class obj;
 
-    int nb = cpp_reflection::reflection_maker<unsigned int()>::invoke(&obj, "some_class", "get_nb");
-    cpp_reflection::reflection_maker<void(unsigned int)>::invoke(&obj, "some_class", "set_nb", nb * 2);
+    int nb = cpp_reflection::reflection_maker<unsigned int()>::invoke(&obj, "reflectable_class", "get_nb");
+    cpp_reflection::reflection_maker<void(unsigned int)>::invoke(&obj, "reflectable_class", "set_nb", nb * 2);
 
-    EXPECT_EQ(2 * nb, cpp_reflection::reflection_maker<unsigned int()>::invoke(&obj, "some_class", "get_nb"));
+    EXPECT_EQ(2 * nb, cpp_reflection::reflection_maker<unsigned int()>::invoke(&obj, "reflectable_class", "get_nb"));
 }
 
 TEST(MemberFunctionReflectionWithInstance, DoExpectedJob) {
-    some_class obj;
-    EXPECT_EQ(42, cpp_reflection::reflection_maker<int(int, int)>::invoke(&obj, "some_class", "add", 20, 22));
+    reflectable_class obj;
+    EXPECT_EQ(42, cpp_reflection::reflection_maker<int(int, int)>::invoke(&obj, "reflectable_class", "add", 20, 22));
 }
 
 TEST(MemberFunctionReflectionWithInstance, UnregisteredFunction) {
     try {
-        some_class obj;
-        cpp_reflection::reflection_maker<void()>::invoke(&obj, "some_class", "unregistered");
+        reflectable_class obj;
+        cpp_reflection::reflection_maker<void()>::invoke(&obj, "reflectable_class", "unregistered");
         FAIL() << "Expected cpp_reflection::reflection_exception";
     }
     catch (const cpp_reflection::reflection_exception& e) {
-        EXPECT_EQ(e.what(), std::string("Function some_class::unregistered is not registered"));
+        EXPECT_EQ(e.what(), std::string("Function reflectable_class::unregistered is not registered"));
     }
     catch (...) {
         FAIL() << "Expected cpp_reflection::reflection_exception";
@@ -68,12 +68,12 @@ TEST(MemberFunctionReflectionWithInstance, UnregisteredFunction) {
 
 TEST(MemberFunctionReflectionWithInstance, StaticMemberFunction) {
     try {
-        some_class obj;
-        cpp_reflection::reflection_maker<void()>::invoke(&obj, "some_class", "static_fct");
+        reflectable_class obj;
+        cpp_reflection::reflection_maker<void()>::invoke(&obj, "reflectable_class", "static_fct");
         FAIL() << "Expected cpp_reflection::reflection_exception";
     }
     catch (const cpp_reflection::reflection_exception& e) {
-        EXPECT_EQ(e.what(), std::string("Function some_class::static_fct can't be called with object"));
+        EXPECT_EQ(e.what(), std::string("Function reflectable_class::static_fct can't be called with object"));
     }
     catch (...) {
         FAIL() << "Expected cpp_reflection::reflection_exception";
@@ -82,7 +82,7 @@ TEST(MemberFunctionReflectionWithInstance, StaticMemberFunction) {
 
 TEST(MemberFunctionReflectionWithInstance, UnregisteredClass) {
     try {
-        some_class obj;
+        reflectable_class obj;
         cpp_reflection::reflection_maker<void()>::invoke(&obj, "unregistered_class", "unregistered");
         FAIL() << "Expected cpp_reflection::reflection_exception";
     }
@@ -97,12 +97,12 @@ TEST(MemberFunctionReflectionWithInstance, UnregisteredClass) {
 
 TEST(MemberFunctionReflectionWithInstance, WrongFunctionPrototype) {
     try {
-        some_class obj;
-        cpp_reflection::reflection_maker<void()>::invoke(&obj, "some_class", "get_nb");
+        reflectable_class obj;
+        cpp_reflection::reflection_maker<void()>::invoke(&obj, "reflectable_class", "get_nb");
         FAIL() << "Expected cpp_reflection::reflection_exception";
     }
     catch (const cpp_reflection::reflection_exception& e) {
-        EXPECT_EQ(e.what(), std::string("Invalid function signature for function some_class::get_nb"));
+        EXPECT_EQ(e.what(), std::string("Invalid function signature for function reflectable_class::get_nb"));
     }
     catch (...) {
         FAIL() << "Expected cpp_reflection::reflection_exception";
