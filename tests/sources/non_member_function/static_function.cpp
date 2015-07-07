3e9ae2a550f7ef9ee75381c8d608fcfcc98a9a0e
Simon Ninon <simon.ninon@etixgroup.com>
2015-07-07 22:46:42 +0200

    patch conflict in tests (class name collision)

--- tests/sources/non_member_function/static_function.cpp
+++ tests/sources/non_member_function/static_function.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <cpp_reflection/cpp_reflection.hpp>
 
-class some_class {
+class some_class_with_statics {
 public:
     static unsigned int get_nb(void) { return 42; }
 
@@ -10,31 +10,31 @@ public:
     static int add(int nb1, int nb2) { return nb1 + nb2; }
 };
 
-REGISTER_CLASS_FUNCTIONS(some_class, (get_nb)(do_something)(add))
+REGISTER_CLASS_FUNCTIONS(some_class_with_statics, (get_nb)(do_something)(add))
 
 TEST(StaticFunction, ReturnGoodValue) {
-    EXPECT_EQ(42, cpp_reflection::reflection_maker<unsigned int()>::invoke("some_class", "get_nb"));
+    EXPECT_EQ(42, cpp_reflection::reflection_maker<unsigned int()>::invoke("some_class_with_statics", "get_nb"));
 }
 
 TEST(StaticFunction, AcceptMultipleParams) {
     int nb = 0;
 
-    cpp_reflection::reflection_maker<void(const std::string&, int&, char, float, int)>::invoke("some_class", "do_something",
+    cpp_reflection::reflection_maker<void(const std::string&, int&, char, float, int)>::invoke("some_class_with_statics", "do_something",
                                                                                                std::string("hello"), nb, 'a', 1.0, 42);
     EXPECT_EQ(42, nb);
 }
 
 TEST(StaticFunction, DoExpectedJob) {
-    EXPECT_EQ(42, cpp_reflection::reflection_maker<int(int, int)>::invoke("some_class", "add", 20, 22));
+    EXPECT_EQ(42, cpp_reflection::reflection_maker<int(int, int)>::invoke("some_class_with_statics", "add", 20, 22));
 }
 
 TEST(StaticFunction, UnregisteredFunction) {
     try {
-        cpp_reflection::reflection_maker<void()>::invoke("some_class", "unregistered");
+        cpp_reflection::reflection_maker<void()>::invoke("some_class_with_statics", "unregistered");
         FAIL() << "Expected cpp_reflection::reflection_exception";
     }
     catch (const cpp_reflection::reflection_exception& e) {
-        EXPECT_EQ(e.what(), std::string("Function some_class::unregistered is not registered"));
+        EXPECT_EQ(e.what(), std::string("Function some_class_with_statics::unregistered is not registered"));
     }
     catch (...) {
         FAIL() << "Expected cpp_reflection::reflection_exception";
@@ -57,11 +57,11 @@ TEST(StaticFunction, UnregisteredClass) {
 
 TEST(StaticFunction, WrongFunctionPrototype) {
     try {
-        cpp_reflection::reflection_maker<void()>::invoke("some_class", "get_nb");
+        cpp_reflection::reflection_maker<void()>::invoke("some_class_with_statics", "get_nb");
         FAIL() << "Expected cpp_reflection::reflection_exception";
     }
     catch (const cpp_reflection::reflection_exception& e) {
-        EXPECT_EQ(e.what(), std::string("Invalid function signature for function some_class::get_nb"));
+        EXPECT_EQ(e.what(), std::string("Invalid function signature for function some_class_with_statics::get_nb"));
     }
     catch (...) {
         FAIL() << "Expected cpp_reflection::reflection_exception";
